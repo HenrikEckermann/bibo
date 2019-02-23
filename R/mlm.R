0d23ac04b6c9d486eddb669c199efe21ecbd05e8
HenrikEckermann <henrikeckermann87@gmail.com>
2019-02-23 13:17:40 +0100

    first commit from bitbucket

--- R/mlm.R
+++ R/mlm.R
@@ -28,7 +28,7 @@ print(start)
 BIBO <- Sys.getenv("BIBO")
 # check if save works 
 check = tibble(test = 1, notest = 2)
-save(check, file = glue("{BIBO}/rdata/check.rds")))
+save(check, file = glue("{BIBO}/rdata/check.rds"))
 print("save worked!!")
 #------ ACTUAL CODING
 
