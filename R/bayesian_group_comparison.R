b30a6234a0e6cfeec7428a938954617ab0be04c6
HenrikEckermann <henrikeckermann87@gmail.com>
2019-03-03 16:53:04 +0100

    refit models with different sigma

--- R/bayesian_group_comparison.R
+++ R/bayesian_group_comparison.R
@@ -49,12 +49,14 @@ get_prior(formula, data = data_imp[[1]], family = student)
 genera <- colnames(data_imp[[1]])[29:158]
 
 # fit models 
-models <- map(genera, function(genus) {
+comparisons <- map(genera, function(genus) {
+  
+    # specify model
     formula_mu <- glue("{genus} ~ time*cc + age_d_s + bf_count_s + sibling*csection + (1|subject_id)") %>% as.formula()
     formula_sigma <- sigma ~ 1 + time*cc
     formula <- bf(formula_mu, formula_sigma)
     
-    # fit example model
+    # fit model
     fit <- brm_multiple(
         data = data_imp,
         family = student(),
@@ -64,12 +66,148 @@ models <- map(genera, function(genus) {
                   prior(normal(0, 0.2), class = b, dpar = sigma), 
                   prior(gamma(2, 0.2), class = nu)),
         cores = 4,
-        file = glue("{BIBO}/models/bayesian_student/student_{genus}")
+        file = glue("{BIBO}/models/bayesian_student/student_sigma_{genus}")
     ) 
+    
+    # create a grid so we can get the mu for each subgroup
+    nd <- with(data_imp[[1]], 
+               expand.grid(cc = levels(cc), time = levels(time),
+                           sibling = levels(sibling), csection = levels(csection)))
+    # we use median value for bf
+    nd$bf_count_s <- median(model.frame(fit)$bf_count_s)
+    # it makes no sense that age remains similar when changing pre to post
+    # so we use the median for pre and post
+    nd <- nd %>% mutate(age_d_s = ifelse(time == "pre", -0.835, 0.626))
+    
+    # only add mu for genera if diagnostics are OK
+    if (return_diag(fit, genus)) {
+      
+        # calulate mu for each subgroup
+        plinp <- posterior_linpred(fit, nd, transform = T,  re.form = ~ 0) %>% as_tibble()
+        colnames(plinp) <- c(
+            "home_pre_nosib_nocsec",
+            "cc_pre_nosib_nocsec",
+            "home_post_nosib_nocsec",
+            "cc_post_nosib_nocsec",
+            "home_pre_sib_nocsec",
+            "cc_pre_sib_nocsec",
+            "home_post_sib_nocsec", 
+            "cc_post_sib_nocsec",
+            "home_pre_nosib_csec",
+            "cc_pre_nosib_csec", 
+            "home_post_nosib_csec",
+            "cc_post_nosib_csec",
+            "home_pre_sib_csec",
+            "cc_pre_sib_csec",
+            "home_post_sib_csec",
+            "cc_post_sib_csec")
+            
+        # mu comparisons (csec group separately)
+        mu_group <- plinp %>%
+            mutate(
+                genus = genus, 
+                
+                # ccpre vs homepre 
+                homepre = (home_pre_nosib_nocsec + home_pre_sib_nocsec)/2,
+                ccpre = (cc_pre_nosib_nocsec + cc_pre_sib_nocsec)/2,
+                ccpre_homepre = ccpre - homepre,
+                
+                # ccpost vs ccpre
+                ccpost = (cc_post_nosib_nocsec + cc_post_sib_nocsec)/2,
+                ccpost_ccpre = ccpost -ccpre,
+                
+                # ccpost vs homepost
+                homepost = (home_post_nosib_nocsec + home_post_sib_nocsec)/2,
+                ccpost_homepost = ccpost - homepost,
+                
+                # homepost vs homepre
+                homepost_homepre = homepost - homepre,
+                
+                # sibling (if csec: home_pre_nosib_csec + cc_pre_nosib_csec + home_post_nosib_csec + cc_post_nosib_csec)
+                nosib = (home_pre_nosib_nocsec + cc_pre_nosib_nocsec + home_post_nosib_nocsec + cc_post_nosib_nocsec)/4,
+                sib = (home_pre_sib_nocsec + cc_pre_sib_nocsec + home_post_sib_nocsec + cc_post_sib_nocsec)/4,
+                sib_nosib = sib - nosib,
+                
+                # csec
+                nocsec = (home_pre_nosib_nocsec + cc_pre_nosib_nocsec + home_post_nosib_nocsec + cc_post_nosib_nocsec +
+                          home_pre_sib_nocsec + cc_pre_sib_nocsec + home_post_sib_nocsec + cc_post_sib_nocsec)/8,
+                csec = (home_pre_nosib_csec + cc_pre_nosib_csec + home_post_nosib_csec + cc_post_nosib_csec +
+                          home_pre_sib_csec + cc_pre_sib_csec + home_post_sib_csec + cc_post_sib_csec)/8,
+                csec_nocsec = csec - nocsec,
+                
+                # sib * csec
+                csec_sib = (home_pre_sib_csec + cc_pre_sib_csec + home_post_sib_csec + cc_post_sib_csec)/4,
+                csec_nosib = (home_pre_nosib_csec + cc_pre_nosib_csec + home_post_nosib_csec + cc_post_nosib_csec)/4,
+                csecsib_csecnosib = csec_sib - csec_nosib
+            ) %>%
+        select(genus, ccpre_homepre, ccpost_ccpre, ccpost_homepost, homepost_homepre, sib_nosib, csec_nocsec, csecsib_csecnosib)
+            
+        
+        
+        # calculate sigma
+        sigma_group <- posterior_samples(fit) %>%
+          mutate(
+            #
+            # homepre
+            home_pre_nosib_nocsec = b_sigma_Intercept,
+            home_pre_nosib_csec = (b_sigma_Intercept + b_sigma_csection1),
+            home_pre_sib_nocsec = (b_sigma_Intercept + b_sigma_sibling1),
+            home_pre_sib_csec = (b_sigma_Intercept + b_sigma_sibling1 + b_sigma_csection1 + `b_sibling1:csection1`),
+            homepre = (home_pre_nosib_nocsec + home_pre_sib_nocsec)/2,
+
+            # homepost
+            home_post_nosib_nocsec = (b_sigma_Intercept + b_sigma_timepost),
+            home_post_nosib_csec = (b_sigma_Intercept + b_sigma_timepost + b_sigma_csection1),
+            home_post_sib_nocsec = (b_sigma_Intercept + b_sigma_timepost + b_sigma_sibling1),
+            home_post_sib_csec = (b_sigma_Intercept + b_sigma_timepost + b_sigma_sibling1 + b_sigma_csection1 + `b_sibling1:csection1`),
+            homepost = (home_post_nosib_nocsec + home_post_sib_nocsec)/2,
+
+            # ccpre
+            cc_pre_nosib_nocsec = (b_sigma_Intercept + b_sigma_ccyes),
+            cc_pre_nosib_csec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_csection1),
+            cc_pre_sib_nocsec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_sibling1),
+            cc_pre_sib_csec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_sibling1 + b_sigma_csection1 + `b_sibling1:csection1`),
+            ccpre = (cc_pre_nosib_nocsec + cc_pre_sib_nocsec)/2,
+
+            # ccpost
+            cc_post_nosib_nocsec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_timepost),
+            cc_post_nosib_csec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_timepost + b_sigma_csection1),
+            cc_post_sib_nocsec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_timepost + b_sigma_sibling1),
+            cc_post_sib_csec = (b_sigma_Intercept + b_sigma_ccyes + b_sigma_timepost + b_sigma_sibling1 + b_sigma_csection1 + `b_sibling1:csection1`),
+            ccpost = (cc_post_nosib_nocsec + cc_post_sib_nocsec)/2,
+
+            # csection
+            nocsec = (cc_post_sib_csec + cc_post_nosib_csec + cc_pre_sib_csec + cc_pre_nosib_csec + home_post_sib_csec + home_post_nosib_csec + home_pre_sib_csec + home_pre_nosib_csec)/8,
+            csec = (cc_post_sib_nocsec + cc_post_nosib_nocsec + cc_pre_sib_nocsec + cc_pre_nosib_nocsec + home_post_sib_nocsec + home_post_nosib_nocsec + home_pre_sib_nocsec + home_pre_nosib_nocsec)/8,
+
+            # siblings 
+            sib = (home_pre_sib_csec + home_pre_sib_nocsec + home_post_sib_csec + home_post_sib_nocsec + cc_pre_sib_csec + cc_pre_sib_nocsec + cc_post_sib_csec + cc_post_sib_nocsec)/8,
+            nosib = (home_pre_nosib_csec + home_pre_nosib_nocsec + home_post_nosib_csec + home_post_nosib_nocsec + cc_pre_nosib_csec + cc_pre_nosib_nocsec + cc_post_nosib_csec + cc_post_nosib_nocsec)/8) %>%
+          mutate_if(is.numeric, exp) %>%
+          mutate(
+            # sigma diff
+            ccpost_homepost = ccpost - homepost,
+            ccpost_ccpre = ccpost - ccpre,
+            homepost_homepre = homepost - homepre, 
+            ccpre_homepre = ccpre - homepre,
+            sib_nosib = sib - nosib, 
+            csec_nocsec = csec - nocsec,
+
+            genus = genus) %>%
+          select(genus, ccpost_homepost, ccpost_ccpre, homepost_homepre, ccpre_homepre, sib_nosib, csec_nocsec)
+        
+        return(list(mu_group, sigma_group)
+        
+          
+    } else {
+        print(glue("{genus} excluded (diagnostics)"))
+        exclude_vector <- c(exclude_vector, genus)
+        return(NA)
+    }
 })
 
 # store vector of succeful fits 
 diagnosed <- map2(models, genera, return_diag)
 
-save(models, diagnosed, file = glue("{BIBO}/rdata/bayesian_student.rds")
+save(comparisons, diagnosed, file = glue("{BIBO}/rdata/bayesian_student.rds")
 
